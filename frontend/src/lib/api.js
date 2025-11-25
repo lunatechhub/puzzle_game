1835174978a71643e25eec2aef86e3b453306974
ERP <lunabhattarai100@gmail.com>
2025-11-25 16:27:51 +0545

    modified api and index class

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:5000",
-  withCredentials: true, // very important for cookies
+  withCredentials: true,   // <— CRITICAL FIX
 });
 
 export default api;
